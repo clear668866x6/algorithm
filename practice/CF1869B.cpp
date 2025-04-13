0e40eda3620bdd0a6989383b9d364f9791fb96eb
Buleqwq <2523739498@qq.com>
2025-04-13 17:35:52 +0800

    1

---
+++ practice/CF1869B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+using PII = pair<int, int>;
+using TII = tuple<int, int, int>;
+#define FOR(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define FOR2(i, a, b, c) for (int i = (int)(a); i <= (int)(b); i += c)
+#define FORD(i, b, a) for (int i = (int)(a); i >= (int)(b); i--)
+#define FORD2(i, b, a, c) for (int i = (int)(a); i >= (int)(b); i -= c)
+#define BSI basic_string<int>
+#define BSPI basic_string<PII>
+#define ALL(a) a.begin(), a.end()
+#define RALL(a) a.rbegin(), a.rend()
+#define VI vector<int>
+#define VII vector<vector<int>>
+#define VPII vector<PII>
+#define lowbit(x) ((x)&(-x))
+#define RE return;
+#define RET return true;
+#define REF return false;
+#define Yes cout << "Yes" << endl;
+#define YES cout << "YES" << endl;
+#define No cout << "No" << endl;
+#define NO cout << "NO" << endl;
+#define pb push_back
+#define fi first
+#define se second
+#define sz size()
+constexpr int N = 2e5 + 10;
+constexpr int mod = 998244353;
+
+int __FAST_IO__ = [](){
+    ios::sync_with_stdio(0), cin.tie(0);
+    cout.tie(0);
+    cout << fixed << setprecision(12);
+    return 0;
+}();
+
+int n,k,a,b;
+int x[N],y[N];
+
+int dis(int x1,int y1,int x2,int y2){
+    return abs(x1-x2)+abs(y1-y2);
+}
+
+void solve() {
+    cin>>n>>k>>a>>b;
+
+    FOR(i,1,n)cin>>x[i]>>y[i];
+
+    int ans=dis(x[a],y[a],x[b],y[b]);
+    int a1=1e18,a2=1e18;
+
+    FOR(i,1,k){
+        a1=min(a1,dis(x[a],y[a],x[i],y[i]));
+        a2=min(a2,dis(x[i],y[i],x[b],y[b]));
+    }
+
+    ans=min(ans,a1+a2);
+
+    cout<<ans<<endl;
+}
+
+signed main() {
+    int Task = 1;
+    for (cin >> Task; Task; Task--) {
+        solve();
+    }
+    return 0;
+}
