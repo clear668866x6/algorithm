55990c0fb4a71ab181e984d12ab44d5164c094b1
Buleqwq <2523739498@qq.com>
2025-05-22 00:46:26 +0800

    1

---
+++ practice/abc362c.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+using PII = pair<int, int>;
+using TII = tuple<int, int, int>;
+#define FOR(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define FOR2(i, a, b, c) for (int i = (int)(a); i <= (int)(b); i += c)
+#define FORD(i, b, a) for (int i = (int)(a); i >= (int)(b); i--)
+#define FORD2(i, b, a, c) for (int i = (int)(a); i >= (int)(b); i -= c)
+#define BSI basic_string<int>
+#define BSPI basic_string<PII>
+#define ALL(a) a.begin(), a.end()
+#define RALL(a) a.rbegin(), a.rend()
+#define VI vector<int>
+#define VII vector<vector<int>>
+#define VPII vector<PII>
+#define lowbit(x) ((x)&(-x))
+#define RE return;
+#define RET return true;
+#define REF return false;
+#define Yes cout << "Yes" << endl;
+#define YES cout << "YES" << endl;
+#define No cout << "No" << endl;
+#define NO cout << "NO" << endl;
+#define pb push_back
+#define fi first
+#define se second
+#define sz size()
+constexpr int N = 2e5 + 10;
+constexpr int mod = 998244353;
+
+int __FAST_IO__ = [](){
+    ios::sync_with_stdio(0), cin.tie(0);
+    cout.tie(0);
+    cout << fixed << setprecision(12);
+    return 0;
+}();
+
+int n,l[N],r[N];
+int tl,tr;
+int ans[N];
+//错误代码：卡在：
+/*
+3
+-10 -8
+-10 -8
+1 20
+
+*/
+
+void solve() {
+    cin>>n;
+
+    FOR(i,1,n){
+        cin>>l[i]>>r[i];
+        tl+=l[i];
+        tr+=r[i];
+    }
+
+    if(tl<=0&&tr>=0){
+        Yes;
+
+        FOR(i,1,n){
+            if(l[i]>0){
+                ans[i]=l[i];
+            }
+            if(r[i]<0){
+                ans[i]=r[i];
+            }
+        }
+
+        int tot=0;
+
+        FOR(i,1,n)tot+=ans[i];
+
+        if(tot>0){
+            FOR(i,1,n){
+                if(!ans[i]){
+                    if(tot+l[i]<=0){
+                        ans[i]=-tot;
+                        break;
+                    }else{
+                        ans[i]=l[i];
+                        tot+=l[i];
+                    }
+                }
+            }
+        }else if(tot<0){
+            FOR(i,1,n){
+                if(!ans[i]){
+                    if(tot+r[i]>=0){
+                        ans[i]=-tot;
+                        break;
+                    }else{
+                        ans[i]=r[i];
+                        tot+=r[i];
+                    }
+                }
+            }
+        }
+        
+        FOR(i,1,n)cout<<ans[i]<<" ";
+
+    }else{
+        No;
+    }
+
+}
+
+signed main() {
+    int Task = 1;
+    for (; Task; Task--) {
+        solve();
+    }
+    return 0;
+}
