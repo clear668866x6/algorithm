8396f7e531cda0dda73abb9a96657ea88e2b3bd6
Buleqwq <2523739498@qq.com>
2025-10-20 23:04:22 +0800

    1

---
+++ practice/nk119225C.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+
+using PII = pair<int, int>;
+using TII = tuple<int, int, int>;
+template<class T> using V = vector<T>;
+using i64 = long long;
+using i128 = __int128;
+using u64 = unsigned long long;
+
+#define FOR(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define FOR2(i, a, b, c) for (int i = (int)(a); i <= (int)(b); i += c)
+#define FORD(i, b, a) for (int i = (int)(a); i >= (int)(b); i--)
+#define FORD2(i, b, a, c) for (int i = (int)(a); i >= (int)(b); i -= c)
+#define ALL(a) a.begin(), a.end()
+#define RALL(a) a.rbegin(), a.rend()
+#define lowbit(x) ((x) & (-x))
+#define RE return;
+#define Yes cout << "Yes" << endl;
+#define YES cout << "YES" << endl;
+#define No cout << "No" << endl;
+#define NO cout << "NO" << endl;
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+
+void solve() {
+    int n;
+    cin >> n;
+    V<int> w(n + 1, 0);
+    FOR(i, 1, n) cin >> w[i];
+
+    int t2 = 0, t3 = 0, t5 = 0;
+
+    FOR(i, 1, n) {
+        while (w[i] % 2 == 0) t2++, w[i] /= 2;
+        while (w[i] % 3 == 0) t3++, w[i] /= 3;
+        while (w[i] % 5 == 0) t5++, w[i] /= 5;
+    }
+
+    cout << min({t5, t2, t3});
+}
+
+signed main() {
+    int Task = 1;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (Task--) {
+        solve();
+    }
+
+    return 0;
+}
