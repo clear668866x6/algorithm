bcd178fef55775b502b123310421f6de34e737e9
Buleqwq <2523739498@qq.com>
2026-02-17 01:08:59 +0800

    1

---
+++ contest/nowcoder/牛客2026年除夕娱乐赛/B.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+
+using PII = pair<int, int>;
+using TII = tuple<int, int, int>;
+template<class T> using V = vector<T>;
+using i64 = long long;
+using i128 = __int128;
+using u64 = unsigned long long;
+
+#define FOR(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define FOR2(i, a, b, c) for (int i = (int)(a); i <= (int)(b); i += c)
+#define FORD(i, b, a) for (int i = (int)(a); i >= (int)(b); i--)
+#define FORD2(i, b, a, c) for (int i = (int)(a); i >= (int)(b); i -= c)
+#define ALL(a) a.begin(), a.end()
+#define RALL(a) a.rbegin(), a.rend()
+#define lowbit(x) ((x) & (-x))
+#define RE return;
+#define Yes cout << "Yes" << endl;
+#define YES cout << "YES" << endl;
+#define No cout << "No" << endl;
+#define NO cout << "NO" << endl;
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+
+void solve() {
+    int n;
+    cin >> n;
+    if (n <= 3) {
+        cout << -1 << endl;
+        RE;
+    }
+
+    if (n == 4) {
+        cout << "2 4 1 3";
+        RE;
+    }
+
+    V<int> c;
+
+    FOR2(i, 4, n, 2) {
+        c.eb(i);
+    }
+
+    sort(RALL(c));
+    for (auto x : c) cout << x << ' ';
+
+    FOR2(i, 1, n, 2) {
+        if (i == 5) {
+            cout << i << " 2 ";
+        } else {
+            cout << i << ' ';
+        }
+    }
+
+    cout << endl;
+}
+
+signed main() {
+    int Task = 1;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (Task--) {
+        solve();
+    }
+
+    return 0;
+}
