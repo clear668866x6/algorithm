e8a4229d8c886222d06ab2207fd9b8bc6a1daaab
Buleqwq <2523739498@qq.com>
2026-03-26 00:20:29 +0800

    1

---
+++ training/codeforces/gym/105891J.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+
+using PII = pair<int, int>;
+using TII = tuple<int, int, int>;
+template<class T> using V = vector<T>;
+using i64 = long long;
+using i128 = __int128;
+using u64 = unsigned long long;
+
+#define FOR(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define FOR2(i, a, b, c) for (int i = (int)(a); i <= (int)(b); i += c)
+#define FORD(i, b, a) for (int i = (int)(a); i >= (int)(b); i--)
+#define FORD2(i, b, a, c) for (int i = (int)(a); i >= (int)(b); i -= c)
+#define ALL(a) a.begin(), a.end()
+#define RALL(a) a.rbegin(), a.rend()
+#define lowbit(x) ((x) & (-x))
+#define RE return;
+#define Yes cout << "Yes" << endl;
+#define YES cout << "YES" << endl;
+#define No cout << "No" << endl;
+#define NO cout << "NO" << endl;
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+
+void solve() {
+    int k;
+    cin >> k;
+    string s;
+    cin >> s;
+    int n = sz(s);
+
+    s = ' ' + s;
+
+    int ans = 0;
+    V<int> vis(n + 1, 0);
+    bool f = false;
+    auto check = [&](V<string> p, int t) {
+        FOR(i, 1, n) {
+            if (i + t - 1 <= n) {
+                string tmp;
+                bool f2 = false;
+                FOR(j, i, i + t - 1) {
+                    if (vis[j]) {
+                        f2 = 1;
+                        break;
+                    }
+                    tmp += s[j];
+                }
+                if (f2) break;
+                bool f3 = false;
+                for (auto j : p) {
+                    if (tmp == j) {
+                        if (k - 4 + t < 0) {
+                            f = 1;
+                            break;
+                        }
+                        f3 = 1;
+                        k -= (4 - t);
+                        ans++;
+                        break;
+                    }
+                }
+                if (f3) {
+                    FOR(j, i, i + t - 1) vis[j] = 1;
+                    i += t - 1;
+                }
+                if (f) break;
+            }
+        }
+    };
+
+    check({"lose"}, 4);
+    check({"lse", "ose", "loe", "los"}, 3);
+    check({"lo", "le", "os", "se", "ls", "oe"}, 2);
+    check({"l", "o", "s", "e"}, 1);
+
+    if (k) {
+        ans += k / 4;
+    }
+
+    cout << ans << endl;
+}
+
+signed main() {
+    int Task = 1;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (Task--) {
+        solve();
+    }
+
+    return 0;
+}
